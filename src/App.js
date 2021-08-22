1853664e8ae864e8b9ea5474f789f7e36da5038e
mohsinayoob <mohsin@devcrew.io>
2021-08-22 19:18:39 +0500

    started project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div class="tall-container-wrapper">
+        <div class="gameplay"><h3>To start game click on board to play as 'X' or click on 'Player O' to go second.</h3></div>
+
+        <table class="play_as">
+          <tr>
+            <td class="player" id="X"><h2>Player X</h2></td>
+            <td class="player" id="O"><h2>Player O</h2></td>
+          </tr>
+        </table>
+
+        <table class="board">
+          <tr>
+            <td class="square" id="0" ><h2></h2></td>
+            <td class="square" id="1"><h2></h2></td>
+            <td class="square" id="2"><h2></h2></td>
+          </tr>
+          <tr>
+            <td class="square" id="3"><h2></h2></td>
+            <td class="square" id="4"><h2></h2></td>
+            <td class="square" id="5"><h2></h2></td>
+          </tr>
+          <tr>
+            <td class="square" id="6"><h2></h2></td>
+            <td class="square" id="7"><h2></h2></td>
+            <td class="square" id="8"><h2></h2></td>
+          </tr>
+        </table>
+
+        <div class="outcome"><h2></h2></div>
+
+        <div class="newGame"><h2>New Game</h2></div>
+      </div>
+    );
+  }
 }
-
 export default App;
